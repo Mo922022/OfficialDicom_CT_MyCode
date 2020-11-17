224b953eb757d57fb735b45797a704969b2d57bc
Mo922022 <Mohmedhamdy922022@gmail.com>
2020-11-17 15:44:40 +0100

    Editing DicomEventAction class

--- DICOM/src/DicomEventAction.cc
+++ DICOM/src/DicomEventAction.cc
@@ -1,59 +1,34 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-// $Id: DicomEventAction.cc 74857 2013-10-23 07:55:55Z gcosmo $
-//
-/// \file medical/DICOM/src/DicomEventAction.cc
-/// \brief Implementation of the DicomEventAction class
-//
-//
-// The code was written by :
-//      *Louis Archambault louis.archambault@phy.ulaval.ca,
-//      *Luc Beaulieu beaulieu@phy.ulaval.ca
-//      +Vincent Hubert-Tremblay at tigre.2@sympatico.ca
-//
-//
-// *Centre Hospitalier Universitaire de Quebec (CHUQ),
-// Hotel-Dieu de Quebec, departement de Radio-oncologie
-// 11 cote du palais. Quebec, QC, Canada, G1R 2J6
-// tel (418) 525-4444 #6720
-// fax (418) 691 5268
-//
-// + Université Laval, Québec (QC) Canada
-//*******************************************************
-
 #include "DicomEventAction.hh"
 #include "G4Event.hh"
 #include "DicomRunAction.hh"
 #include "DicomRun.hh"
 
+// New Add
+#include <G4EventManager.hh>
+#include <G4VVisManager.hh>
+#include "G4SDManager.hh"
+#include <G4UImanager.hh>
+#include <G4ios.hh>
+#include <vector>
+#include <map>
+#include <TVector3.h>
+#include <TString.h>
+#include <G4SystemOfUnits.hh>
+#include "PhantomHit.hh"
+#include "HistoManager.hh"
+
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-DicomEventAction::DicomEventAction()
- : G4UserEventAction(),
-   fDrawFlag("all")
-{ }
+DicomEventAction::DicomEventAction(HistoManager* histo):dataManager(histo),
+ EventNo(0) 
+   //fDrawFlag("all")
+{ 
+	Energy= new std::vector<double>;
+	Time= new std::vector<double>;
+	particleID=new std::vector<int>;
+	Position = new std::vector<TVector3>;
+	Momentum = new std::vector<TVector3>;
+}
+
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 DicomEventAction::~DicomEventAction()
@@ -62,9 +37,57 @@ DicomEventAction::~DicomEventAction()
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 void DicomEventAction::BeginOfEventAction(const G4Event* evt)
 {
-  G4cout << "EV: " << evt->GetEventID() << G4endl;
+    EventNo=GetEventno();
+    //std::cout << "Event no " << EventNo << std::endl;
+	Energy->clear();
+	Time->clear();
+	particleID->clear();
+	Position->clear();
+	Momentum->clear();
+	dataManager->IncreasePrimaries();
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-void DicomEventAction::EndOfEventAction(const G4Event*)
-{ }
+void DicomEventAction::EndOfEventAction(const G4Event* evt)
+{
+	
+G4SDManager * SDman = G4SDManager::GetSDMpointer();
+
+	PhantomHitsCollection* SHC = 0;
+
+	G4HCofThisEvent* HCE = evt->GetHCofThisEvent();
+	
+	if (HCE){
+	int nPhantom=HCE->GetNumberOfCollections();	
+		//G4cout << "Number of Collections" << nScins << G4endl;
+		for(int j=0;j<nPhantom;j++){
+
+			//G4cout << "Current Collection ID" << ScinID << G4endl;
+			SHC = (PhantomHitsCollection*)(HCE->GetHC(j));
+
+			if (SHC){
+
+				int phantom_hit = SHC->entries();
+				for (G4int i=0;i<phantom_hit;i++){
+					Energy->push_back((*SHC)[i]->GetEkin());
+					Time->push_back((*SHC)[i]->GetTime());
+					particleID->push_back((*SHC)[i]->GetParticleID());
+					Position->push_back((*SHC)[i]->GetPosition());
+					Momentum->push_back((*SHC)[i]->GetMomentum());
+				}
+				if(phantom_hit!=0)dataManager->SaveSecondaries(particleID,Energy,Time,Position,Momentum);
+			}
+
+
+		}
+	}	
+	
+	
+}
+//***********************Helper Functions ***************
+
+G4int DicomEventAction::GetEventno() {
+	G4int evno = fpEventManager->GetConstCurrentEvent()->GetEventID();
+	return evno ;
+}
+
